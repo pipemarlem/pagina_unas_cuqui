3e80dd7df2901c333ee17a8ef2a556b19bcfa6c9
pipemarlem <felipemartinezlemos@gmail.com>
2025-07-23 20:36:09 -0300

    borrar todo

--- app/hooks/useClientAuthSession.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import { useContext } from 'react';
-import { WixSessionContext } from '@app/components/Provider/WixBookingsClientProvider';
-import { WixSession } from '@app/model/auth/auth';
-
-export const useClientAuthSession = (): WixSession => {
-  return useContext(WixSessionContext)!;
-};
--- app/hooks/useServerAuthSession.tsx
+++
@@ -1,8 +0,0 @@
-import { WixSession } from '@app/model/auth/auth';
-import { getServerWixClient } from '@app/model/auth/wix-client.server';
-import { cookies as nextCookies } from 'next/headers';
-export const useServerAuthSession = (): WixSession => {
-  return {
-    wixClient: getServerWixClient({ cookieStore: nextCookies() }),
-  };
-};
